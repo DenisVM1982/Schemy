bc028c8e84b2940eb3e1b256e03ff1b65307ec73
Denis Mayorov <denisvalerievichmayorov@gmail.com>
2019-04-08 19:39:42 +0300

    jldjflj

---
+++ app/src/main/java/com/example/kitkat/DomennyActivity.java
@@ -0,0 +1,13 @@
+package com.example.kitkat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DomennyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domenny);
+    }
+}
