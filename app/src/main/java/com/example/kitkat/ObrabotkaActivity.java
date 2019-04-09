497e4457a6b66a1683d70b2a0a8f2dfec35b1535
Denis Mayorov <denisvalerievichmayorov@gmail.com>
2019-04-09 09:35:19 +0300

    schema_0.23

---
+++ app/src/main/java/com/example/kitkat/ObrabotkaActivity.java
@@ -0,0 +1,13 @@
+package com.example.kitkat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObrabotkaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obrabotka);
+    }
+}
