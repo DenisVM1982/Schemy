fb461c61dafd957c06665e4b84a3c19ab3edb236
Denis Mayorov <denisvalerievichmayorov@gmail.com>
2019-04-09 08:57:34 +0300

    schemy_0.21

---
+++ app/src/main/java/com/example/kitkat/HodMaloyTelegiActivity.java
@@ -0,0 +1,21 @@
+package com.example.kitkat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+public class HodMaloyTelegiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.hod_maloy_telegi);
+        photoView.setImageResource(R.drawable.hod_maloy_telegi);
+    }
+}
